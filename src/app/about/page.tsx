e5cba7a9c5f9749783478cd3c0354a1bf2988756
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-09-26 20:07:07 -0700

    started working on about page layout and content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import Image from 'next/image'
+//static image imports
+import churchesOfChrist from '@/public/banners/about/churchesOfChrist.jpg'
+import members from '@/public/banners/about/members.jpg'
+import plea from '@/public/banners/about/plea.jpg'
 
 const About = () => {
   return (
     <div className='page-container'>
       <h1>About The Church</h1>
+      <section className='flex flex-col items-center'>
+        <Image src={churchesOfChrist} alt='decorative blue banner for churches of christ section' />
+        <p className='bg-blue-200 font-bold'>By: <span className='text-blue-600'>Batsell Barrett Baxter</span></p>
+      </section>
+      <section className='flex flex-col items-center italic gap-4 font-bold'>
+        <Image src={members} alt='decorative blue banner for members of church section' />
+        <ul className='list-disc flex flex-col gap-1 text-sm list-inside'>
+          <li className='text-blue-600'><span className='text-black'>Seek to duplicate the non-denominational church of the first century.</span></li>
+          <li className='text-blue-600'><span className='text-black'>Take the bible as our supreme and only guide.</span></li>
+          <li className='text-blue-600'><span className='text-black'>Reject doctrines and practices that were introduced after the close of the new testament.</span></li>
+          <li className='text-blue-600'><span className='text-black'>Seek to be Christians only.</span></li>
+          <li className='text-blue-600'><span className='text-black'>Believe this is the one and only way to unite all who are seeking to follow Christ.</span></li>
+        </ul>
+        <p>{`
+          The apostle Peter once wrote, "Sanctify in your hearts Christ as Lord, being ready always
+          to give answer to every man that asks you a reason concerning the hope that is in you, with
+          meekness and fear" (I Peter 3:15.)  Christians are admonished to be ready at all times to give
+          a reason for the faith which they hold.`}
+        </p>
+      </section>
+      <section className='flex flex-col items-center'>
+        <Image src={plea} alt='decorative blue banner for distinctive plea section' />
+        <p>
+          {
+            `It is primarily a plea for religious unity based upon the bible. In a divided religious world it is
+          believed that the bible is the only possible common denominator upon which most, if not all, of the
+          God fearing people of the land can unite. This is an appeal to go back to the bible.  It is a plea to
+          speak where the bible speaks and to remain silent where the bible is silent in all matters that pertain
+          to religion.  It further emphasizes that in everything religious, there must be a "Thus saith the Lord"
+          for all that is done.  The objective is religious unity of all believers in Christ.  The basis is the new
+          testament.`
+          }
+        </p>
+      </section>
     </div>
   )
 }
--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -2,39 +2,31 @@
 import React from 'react'
 import Link from 'next/link'
 import Hamburger from './Hamburger'
-import ResizeHook from '../../utils/ResizeHook'
 
 const Nav = () => {
-  const windowWidth = ResizeHook()
-  //The hamburger component needs to be wrapped in a parent container with fixed and
-  //width 100 so we can move the hamburger menu to the right side of the screen
-  if (windowWidth === 'loading') return null
   return (
-    <div className='z-10'>
-      {windowWidth < 648 ?
-        <div className='fixed w-full'>
-          <Hamburger />
-        </div>
-        :
-        <div className='flex w-full justify-evenly text-sm h-16 bg-slate-900 text-slate-50 items-end pb-2 gap-6 sm:flex-row md:text-lg'>
-          <Link href='/'>
-            HOME
-          </Link>
-          <Link href='/about'>
-            WHAT&apos;S THE CHURCH ABOUT?
-          </Link>
-          <Link href='/contact'>
-            CONTACT US
-          </Link>
-          <Link href='/newsletter'>
-            NEWSLETTER
-          </Link>
-          <Link href='/meet'>
-            MEET THE CONGREGATION
-          </Link>
-        </div>
-      }
-    </div>
+    <nav className='z-10'>
+      <div className='fixed w-full sm:hidden'>
+        <Hamburger />
+      </div>
+      <div className='flex w-full justify-evenly text-sm h-16 bg-slate-900 text-slate-50 items-end pb-2 gap-6 sm:flex-row md:text-lg max-sm:hidden'>
+        <Link href='/'>
+          HOME
+        </Link>
+        <Link href='/about'>
+          WHAT&apos;S THE CHURCH ABOUT?
+        </Link>
+        <Link href='/contact'>
+          CONTACT US
+        </Link>
+        <Link href='/newsletter'>
+          NEWSLETTER
+        </Link>
+        <Link href='/meet'>
+          MEET THE CONGREGATION
+        </Link>
+      </div>
+    </nav>
   )
 }
 
